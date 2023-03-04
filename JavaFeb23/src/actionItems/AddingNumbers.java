e84cdadecec4d02deaf0da6729f85b93c6c795fd
pszoe <zoelixue123@gmail.com>
2023-03-04 11:49:53 -0500

    Action Items Added

---
+++ JavaFeb23/src/actionItems/AddingNumbers.java
@@ -0,0 +1,25 @@
+package actionItems;
+
+public class AddingNumbers {
+
+	public static void main(String[] args) {
+
+		// adding numbers from 0 to 15 using loop
+		// for loop
+		int sum = 0;
+		for (int i = 0; i <= 15; i++) {
+			sum = sum + i;
+		}
+		System.out.println("The result is " + sum);
+
+		// while loop
+		int sum1 = 0;
+		int i = 0;
+		while (i <= 15) {
+			sum1 = sum1 + i;
+			i++;
+		}
+		System.out.println("The result is " + sum1);
+	}
+
+}
